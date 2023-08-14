352346e82242a4acd7bb78592c6368043800c2aa
Felipe Almeida <felipepereira937@gmail.com>
2023-08-14 16:12:28 -0300

    Configuração de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserDataComponent } from './users/user-data/user-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-list', component: UserListComponent },
+  { path: 'user-data', component: UserDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
