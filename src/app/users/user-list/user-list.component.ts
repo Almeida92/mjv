b63e556aac4f10f784aa64553960590be0acf429
Felipe Almeida <felipepereira937@gmail.com>
2023-08-14 17:04:10 -0300

    Criando tela de listagem

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserModel } from '../user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent {
-
+  userList: Array<UserModel> = [
+    {
+      id: "60d0fe4f5311236168a109fa",
+      title: "ms",
+      firstName: "Ann",
+      lastName: "Mason",
+      picture: "https://randomuser.me/api/portraits/med/women/18.jpg"
+    },
+    {
+      id: "60d0fe4f5311236168a109fb",
+      title: "mr",
+      firstName: "Sohan",
+      lastName: "Pierre",
+      picture: "https://randomuser.me/api/portraits/med/men/77.jpg"
+    },
+    {
+      id: "60d0fe4f5311236168a109fc",
+      title: "mr",
+      firstName: "Gonzaga",
+      lastName: "Ribeiro",
+      picture: "https://randomuser.me/api/portraits/med/men/10.jpg"
+    },
+    {
+      id: "60d0fe4f5311236168a109ff",
+      title: "mrs",
+      firstName: "Josefina",
+      lastName: "Calvo",
+      picture: "https://randomuser.me/api/portraits/med/women/3.jpg"
+    },
+    {
+      id: "60d0fe4f5311236168a10a00",
+      title: "mrs",
+      firstName: "Els",
+      lastName: "Ijsseldijk",
+      picture: "https://randomuser.me/api/portraits/med/women/75.jpg"
+    },
+  ]
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserDataComponent } from './user-data/user-data.component';
-import {  MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import {  MatListModule} from '@angular/material/list';
   ],
   imports: [
     CommonModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class UsersModule { }
